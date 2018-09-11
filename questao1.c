c1ab791a40e35d2e0195dcb9f33d50e3b3328ce1
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2018-09-10 21:42:20 -0300

    Create questao1.c

---
+++ questao1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("O uso de ponteiros permite acessar um endereço de memoria. \nPor causa disso, ao resolver um problema, eh possivel fazer programas cada vez mais rapidos e complexos. O que eh um recurso necessario na Engenharia.\n");
+    return 0;
+}
+
