af783a1e91d0ac1e0aacf1962c07af4b3c0f1672
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2018-09-10 21:46:07 -0300

    Create questao9.c

---
+++ questao9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*/
+int main(){
+  int vet[] = {4,9,13}; // inicializa um vetor de inteiros com os valores 4,9,13
+  int i; // declara uma variavel inteira i
+
+  for(i=0;i<3;i++){ // percorre todo o vetor de tamanho 3
+    printf("%d ",*(vet+i)); // imprime cada valor do vetor (4,9,13) seguido de espaço
+  }
+}
+/*/
+
+/*/
+int main(){
+  int vet[] = {4,9,13}; // inicializa um vetor de inteiros com os valores 4,9,13
+  int i; // declara uma variavel inteira i
+  for(i=0;i<3;i++){// percorre todo o vetor de tamanho 3
+    printf("%X ",vet+i);// imprime a posição de cada elemento do vetor
+  }
+}
+/*/
