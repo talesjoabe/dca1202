6f68735abd4bf1f10b18cc952d885d281cf95d74
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2018-09-10 21:49:17 -0300

    Create questao12.c

---
+++ questao12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// QUESTAO 12
+
+int main()
+{
+    float aloha[10], coisas[10][5], *pf, value = 2.2;
+    int i=3;
+
+    // os seguintes comandos são validos:
+
+    //aloha[2] = value;
+    //printf("%f", aloha);
+    //coisas[4][4] = aloha[3];
+    //pf = aloha;
+
+    return 0;
+}
