b3322bab545bae5d6c4bce4721e0432141c7d6bb
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2018-09-10 21:51:26 -0300

    Create questao17.c

---
+++ questao17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    // utilizei os mecanismos de verificação de tempo de execução nas questões anteriores.
+    // com a seguinte estrutura:
+
+    // clock_t Ticks[2];
+    // Ticks[0] = clock();
+    // O código a ter seu tempo de execução medido ficaria neste ponto.
+    // Ticks[1] = clock();
+    // double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    // printf("Tempo gasto: %g ms.", Tempo);
+    // getchar();
+
+    // quanto ao tempo de execução. houve uma diferença considerável entre a questao 15 e 16.
+    // enquanto a 15 executa em torno de 1 ms, a 16 em torno de 1000ms
+    /*/
+    printf("Hello World!\n");
+    return 0;
+}
